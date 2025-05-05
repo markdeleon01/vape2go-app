85ccb56821d64302598407638ea589c60cd9f840
Mark de Leon <mark_deleon@hotmail.com>
2025-05-05 17:51:12 +0800

    Create Product API endpoint

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,5 +1,4 @@
 import { Product, ProductStore } from '@/app/lib/product'
-import { NextResponse } from 'next/server'
 
 export async function GET() {
     console.log('GET /products')
@@ -7,10 +6,36 @@ export async function GET() {
     const store = new ProductStore()
     try {
         const products: Product[]  = await store.index() as Product[]
-        console.log(JSON.stringify(products))
-        return NextResponse.json(products)
+        console.log('products='+JSON.stringify(products))
+        return Response.json(products)
     } catch (error) {
         console.error(error)
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+        return Response.json({ error: 'Internal Server Error' }, { status: 500 })
+    }
+}
+
+export async function POST(request: Request) {
+    console.log('POST /products')
+    const data = await request.json()
+    try {
+        const store = new ProductStore()
+        const newProduct: Product = {
+            name: data.name,
+            brand: data.brand,
+            price: data.price,
+            description: data.description,
+            imageUrl: data.imageUrl,
+            flavourName: data.flavourName,
+            puffsNumber: data.puffsNumber,
+            ingredients: data.ingredients,
+            typeProduct: data.typeProduct,
+            quantity: data.quantity
+        }
+        const res = await store.create(newProduct)
+        console.log('newProduct='+JSON.stringify(res))
+        return Response.json(res)
+    } catch (error) {
+        console.error(error)
+        return Response.json({ error: 'Internal Server Error' }, { status: 500 })
     }
 }
--- app/lib/product.ts
+++ app/lib/product.ts
@@ -21,11 +21,24 @@ export class ProductStore {
     // method to return a list of Product
     async index() {
         try {
-            console.log('ProductStore::index()')
+            //console.log('ProductStore::index()')
             const data = await sql`SELECT * FROM products;`
+            //console.log('data='+JSON.stringify(data))
             return data
         } catch (error) {
             throw new Error('Cannot get list of Product: ' + error)
+        }
+    }
+
+    // method to create a Product
+    async create(p: Product) {
+        try {
+            //console.log('ProductStore::create(p:Product)')
+            const data = await sql`INSERT INTO products (name, brand, price, description, imageUrl, flavourName, puffsNumber, ingredients, typeProduct, quantity) VALUES(${p.name}, ${p.brand}, ${p.price}, ${p.description}, ${p.imageUrl}, ${p.flavourName}, ${p.puffsNumber}, ${p.ingredients}, ${p.typeProduct}, ${p.quantity}) RETURNING *`
+            //console.log('data='+JSON.stringify(data))
+            return data
+        } catch (error) {
+            throw new Error('Cannot create Product: ' + error)
         }
     }
 }
