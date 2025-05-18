b48a393c29097f7454bd354320caa26493ab9b0e
Mark de Leon <mark_deleon@hotmail.com>
2025-05-18 20:47:21 +0800

    nextjs-toploader

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from './ui/navbar';
+import TopLoader from "./ui/topLoader"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TopLoader/>
         <NavBar />
         {children}
       </body>
