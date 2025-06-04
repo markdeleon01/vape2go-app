2b5f98f809dd1016a729650d2694c4a6275cf19f
Mark de Leon <mark_deleon@hotmail.com>
2025-06-04 16:11:05 +0800

    Add loading indicator while getting products list.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,7 +5,7 @@ import { getProducts } from '@/app/lib/service'
 import Loading from '@/app/ui/loading'
 import ErrorComponent from '@/app/ui/error'
 
-import { Suspense } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
 import dynamic from 'next/dynamic'
@@ -16,14 +16,29 @@ const ProductsList = dynamic(
 )
 
 export default function ProductsPage() {
+	const [isLoading, setIsLoading] = useState(true)
+
 	const products = getProducts()
+	
+	useEffect(() => {
+		// Check if products are loaded
+		products.then(() => {
+			setIsLoading(false)
+		}).catch((error) => {
+			console.error('Error loading products:', error)
+			setIsLoading(false)
+			throw new Error('Error loading products')
+		})
+	}, [products])
 
 	return (
 		<div className='grid justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]'>
 			<main className='flex flex-col items-center sm:items-start'>
 				<ErrorBoundary fallback={<ErrorComponent error={new Error('Unable to load page.') }/> }>
 					<Suspense fallback={<Loading />}>
-						<ProductsList products={products} />
+						{ isLoading && <Loading />}
+						{ !isLoading && <ProductsList products={products} />}
+						
 					</Suspense>
 				</ErrorBoundary>
 			</main>
